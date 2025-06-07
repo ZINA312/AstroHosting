40f0b0f2c2b3df161b566d0ed8a1ca9c00f4cb3f
Alexey Kudosh <kudoschlexa@yandex.ru>
2025-06-07 17:23:10 +0300

    Configuration applies from assembly, removed DBSets

--- Backend/AstroHosting.Persistence/Data/AstroHostingDBContext.cs
+++ Backend/AstroHosting.Persistence/Data/AstroHostingDBContext.cs
@@ -1,34 +1,19 @@
-﻿using AstroHosting.Core.Entities;
-using AstroHosting.Persistence.Configurations;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace AstroHosting.Persistence.Data
 {
     public class AstroHostingDBContext : DbContext
     {
-        public AstroHostingDBContext(DbContextOptions<AstroHostingDBContext> options) : base(options) 
-        {
-        }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<Subscription> Subscriptions { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Like> Likes { get; set; }
-        public DbSet<Equipment> Equipment { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<PostEquipment> PostEquipments { get; set; }
+        public AstroHostingDBContext(DbContextOptions<AstroHostingDBContext> options) 
+            : base(options) 
+        { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-            modelBuilder.ApplyConfiguration(new SubscriptionConfiguration());
-            modelBuilder.ApplyConfiguration(new PostConfiguration());
-            modelBuilder.ApplyConfiguration(new LikeConfiguration());
-            modelBuilder.ApplyConfiguration(new EquipmentConfiguration());
-            modelBuilder.ApplyConfiguration(new CommentConfiguration());
-            modelBuilder.ApplyConfiguration(new PostEquipmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
